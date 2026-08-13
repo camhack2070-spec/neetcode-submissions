c015a2f6f61ff513a3d2ef7ccdc2663f4e7beef9
frieren-no-coder16126 <camhack2070@gmail.com>
2026-08-13 20:04:43 +0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Hi Phu"
+print(type(variable))
+variable = [1,3,2]
+print(type(variable))
