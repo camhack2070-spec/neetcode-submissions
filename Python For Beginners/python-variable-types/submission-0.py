7aba977e7637a069dc97d52547731b06178f33b7
frieren-no-coder16126 <camhack2070@gmail.com>
2026-08-13 20:01:50 +0700

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,10 @@
+integer_type = 10
+float_type = 10.12
+bool_type = False
+string_type = "hello"
+list_type = [432,13432,432]
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
