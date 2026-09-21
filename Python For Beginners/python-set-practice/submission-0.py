1f390eed0425c0637d25300d5f5704054af05a8a
frieren-no-coder16126 <camhack2070@gmail.com>
2026-09-21 15:34:20 +0700

    Add: python-set-practice - submission-0

---
+++ Python For Beginners/python-set-practice/submission-0.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def contains_duplicate(words: List[str]) -> bool:
+    for i in range(len(words) - 1):
+        for j in range(i + 1, len(words)):
+            if words[i] == words[j]:
+                return True
+
+    return False 
+    pass
+
+# do not modify code below this line
+print(contains_duplicate(["hello", "world", "hello"]))
+print(contains_duplicate(["hello", "world", "i", "am", "great"]))
+print(contains_duplicate(["hello", "hello", "hello"]))
+print(contains_duplicate(["Hello", "hellooo", "hello"]))
